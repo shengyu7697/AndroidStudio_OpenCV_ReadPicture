cc4ab3b7c6afdf6c5a6c26263c42dd8217e4202c
shengyu <shengyu7697@gmail.com>
2016-03-01 11:19:20 +0800

    add ImageView (show gray image)

--- app/src/main/java/com/android/opencv/androidstudio_opencv_readpicture/MainActivity.java
+++ app/src/main/java/com/android/opencv/androidstudio_opencv_readpicture/MainActivity.java
@@ -1,12 +1,24 @@
 package com.android.opencv.androidstudio_opencv_readpicture;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        iv = new ImageView(this);
+        setContentView(iv);
+
+        Bitmap bmp;
+        bmp = Bitmap.createBitmap(640, 480, Bitmap.Config.ARGB_8888);
+        bmp.eraseColor(Color.GRAY);
+        iv.setImageBitmap(bmp);
     }
 }
