ed275d7963135e26943d2f90b847077742542e5b
shengyu <shengyu7697@gmail.com>
2016-03-01 11:15:49 +0800

    init

---
+++ app/src/main/java/com/android/opencv/androidstudio_opencv_readpicture/MainActivity.java
@@ -0,0 +1,12 @@
+package com.android.opencv.androidstudio_opencv_readpicture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
